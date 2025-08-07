bfc2a3aee12b33501e8a922d90cc016f411fbd88
Avijeet Palit <avijeet.palit07@gmail.com>
2025-08-08 02:52:53 +0530

    ALl u can do is try more & fail better

--- core/schema_analyze.py
+++ core/schema_analyze.py
@@ -5,6 +5,7 @@ from .llm_interface import call_llm
 from .logger_service import log
 from .token_estimator import estimate_tokens
 from jsonschema import Draft7Validator, exceptions as jsonschema_exceptions
+from typing import Dict, Any, Tuple, List, Optional
 
 def is_valid_schema(schema_json: Dict[str, Any]) -> Tuple[bool, Optional[str]]:
     """
